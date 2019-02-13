350b95bb3229e436677cfbd82cb34e1181015989
Sergey Babak <s.babak@scalr.com>
2019-02-13 16:48:53 +0200

    update variables

--- gcp/virtual-machine/main.tf
+++ gcp/virtual-machine/main.tf
@@ -1,15 +1,15 @@
 provider "google" {
   credentials = "${file("account.json")}"
   project     = "${var.project}"
-  region      = "${var.region}"
+  zone      = "${var.zone}"
 }
- 
+
 resource "google_compute_instance" "terraform-test-instance" {
   count        = 1
   name         = "terraform-test-instance-${count.index + 1}"
   machine_type = "f1-micro"
-  zone         = "${var.region}"
- 
+  zone         = "${var.zone}"
+
   boot_disk {
     initialize_params {
       image = "debian-7-wheezy-v20160301"
--- gcp/virtual-machine/variables.tf
+++ gcp/virtual-machine/variables.tf
@@ -1,7 +1,3 @@
-variable "region" {
-  default = "europe-west1"
-}
-
 variable "zone" {
   default = "europe-west1-d"
 }
