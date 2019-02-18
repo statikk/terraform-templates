b2b97a85a909ee74775e772d2f48a2b97948ad5c
Sergey Babak <s.babak@scalr.com>
2019-02-18 18:22:17 +0200

    wip

--- gcp/virtual-machine/main.tf
+++ gcp/virtual-machine/main.tf
@@ -16,7 +16,7 @@ resource "google_compute_instance" "terraform-test-instance" {
     }
   }
 
-  tags = ["test-instance-2"]
+  tags = ["test-instance-3"]
 
   network_interface {
     network = "default"
