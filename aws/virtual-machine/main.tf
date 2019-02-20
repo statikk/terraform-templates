f070887383e3aed554eb73ec1d57ebfb2a04c796
Sergey Babak <s.babak@scalr.com>
2019-02-20 21:58:51 +0200

    wip

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -15,13 +15,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-/*
 resource "aws_instance" "terraform-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "${var.instance_type}"
 
   tags = {
     Name = "test-instance"
+    timestamp = "${timestamp()}"
+  }
+
+  lifecycle {
+    ignore_changes = ["tags"]
   }
 }
-*/
