143d6604b7ab9202cb0b943a03f24596bc4158c6
Sergey Babak <s.babak@scalr.com>
2019-08-27 13:11:56 +0300

    test commit 006

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -32,3 +32,18 @@ resource "aws_instance" "terraform-test-instance" {
     ignore_changes = ["tags"]
   }
 }
+
+
+resource "aws_instance" "terraform-test-instance2" {
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "${var.instance_type}"
+
+  tags = {
+    Name = "test-instance"
+    timestamp = "${timestamp()}"
+  }
+
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
+}
