a9ded8746635e255a0e43eaac9ebf5160e78df82
Sergey Babak <s.babak@scalr.com>
2019-08-30 16:18:23 +0300

    test commit 007

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -18,7 +18,6 @@ data "aws_ami" "ubuntu" {
 }
 
 
-
 # resource "aws_instance" "terraform-test-instance" {
 #   ami             = "${data.aws_ami.ubuntu.id}"
 #   instance_type   = "${var.instance_type}"
