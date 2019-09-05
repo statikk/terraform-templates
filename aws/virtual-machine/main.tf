344575681747ee16fb655eadbe045b15e18add7f
statikk <greedmysin@gmail.com>
2019-09-05 15:38:43 +0300

    Update main.tf

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -19,16 +19,16 @@ data "aws_ami" "ubuntu" {
 }
 
 
-# resource "aws_instance" "terraform-test-instance" {
-#   ami             = "${data.aws_ami.ubuntu.id}"
-#   instance_type   = "${var.instance_type}"
+resource "aws_instance" "terraform-test-instance" {
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "${var.instance_type}"
 
-#   tags = {
-#     Name = "test-instance"
-#     timestamp = "${timestamp()}"
-#   }
+  tags = {
+    Name = "test-instance"
+    timestamp = "${timestamp()}"
+  }
 
-#   lifecycle {
-#     ignore_changes = ["tags"]
-#   }
-# }
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
+}
