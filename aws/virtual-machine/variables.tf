dff1c58729f806a332b26e7b23716a4fec13f8d3
statikk <greedmysin@gmail.com>
2019-09-06 16:40:22 +0300

    Update variables.tf

--- aws/virtual-machine/variables.tf
+++ aws/virtual-machine/variables.tf
@@ -5,3 +5,11 @@ variable "region" {
 variable "instance_type" {
   default = "m3.medium"
 }
+
+resource "aws_instance" "s-tf" {
+  ami = var.image_id
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  tags = {
+    test-env-owner = "<o.timoshyn@scalr.com>"
+}
