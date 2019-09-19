6108fa15cfc09d34b6027ab682955979129748cd
statikk <greedmysin@gmail.com>
2019-09-19 10:40:21 +0300

    Update main.tf

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "terraform-test-instance" {
   instance_type   = "${var.instance_type}"
 
   tags = {
-    Name = "test-instance"
+    Name = "ST-test-instance"
     timestamp = "${timestamp()}"
     test-env-owner = "o.timoshyn@scalr.com"
   }
