e54557226fe0652f15de2c35a0f8a286a8e4ff9a
Oleksandr Timoshyn <o.timoshyn@scalr.com>
2021-03-31 14:51:36 +0300

    commit01

---
+++ aws/virtual-machine/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+
+resource "aws_instance" "terraform-test-instance" {
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "${var.instance_type}"
+
+  tags = {
+    Name = "SashaT-instance-000"
+    timestamp = "${timestamp()}"
+    test-env-owner = "o.timoshyn@scalr.com"
+    owner = "o.timoshyn"
+  }
+  
+
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
+}
---
+++ aws/virtual-machine/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "m3.medium"
+}
